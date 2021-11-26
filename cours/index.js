d526dee14e05a84f74d0832f3a879b493d3024a8
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-26 13:32:51 +0100

    installation du json-server

--- cours/index.js
+++ cours/index.js
@@ -27,3 +27,24 @@ fetch("data.txt")
 fetch("data.json")
     .then((res) => res.json())
     // .then((data) => console.log(data["workbench.colorTheme"]));
+
+const myHeaders = new Headers();
+
+const init = {
+    method: "GET",
+    headers: myHeaders,
+    mode: "cors",
+    cache: "default",
+};
+
+// fetch("data.json", init)
+//     .then((res) => console.log(res));
+
+// CRUD => Create (POST), read (GET), update (PUT), Delete (DELETE)
+
+// Installation du Json Server
+// npm -> gestionnaire de dépendances pour Node JS
+// installation : npm init -y
+// npm i -g json-server
+// faire tourner le serveur: json-server --w <nom-du-fichier>
+// Le serveur va travailler avec "db.json" comme si c'était une BDD distante
