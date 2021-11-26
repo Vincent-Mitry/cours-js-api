c08d8e3b9178834e4e0a669a3aa1c50772d4beec
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-26 12:39:50 +0100

    cours ok

---
+++ cours/index.js
@@ -0,0 +1,29 @@
+// XMLHttpRequest
+
+function reqListener(){
+    // console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+// req.open('get', 'data.txt', true);
+req.open('get', 'https://api.blablagues.net/?rub=blagues', true);
+req.send();
+
+//-------
+// FETCH
+//-------
+
+// fetch("monlien", "objet d'options")
+//     .then((response) => {
+    // response
+//     })
+//     .catch((err) => console.log(err));
+
+fetch("data.txt")
+    .then((res) => res.text())
+    // .then((data) => console.log(data));
+
+fetch("data.json")
+    .then((res) => res.json())
+    // .then((data) => console.log(data["workbench.colorTheme"]));
